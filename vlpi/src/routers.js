ab0a9b59afb9023ab6f01f4156addd0f9834eecb
Ivan Kostiv <ivankostiv.pz@gmail.com>
2021-01-10 14:05:53 +0200

    feat: added home page

--- vlpi/src/routers.js
+++ vlpi/src/routers.js
@@ -1,26 +1,25 @@
 import React, {Component} from 'react';
 import {BrowserRouter} from 'react-router-dom';
-import {Route, Switch} from 'react-router';
+import {Route, Switch, Router} from 'react-router';
 
 import HomePage from "./components/HomePage/HomePage";
 import TaskListPage from "./components/TaskListPage/TaskListPage";
 import GoJsDemo from "./components/GoJsDemo/GoJsDemo";
 import Header from "./components/Header/Header";
+import history from "./history"
 
 class Routers extends Component {
 
     render() {
         return(
-            <BrowserRouter>
+            <Router history={history}>
                 <Header />
                 <Switch>
-                    <Route path='/' exact
-                           component={HomePage}
-                    />
+                    <Route path='/' exact component={HomePage} />
                     <Route path='/task-list' exact component={TaskListPage} />
                     <Route path='/gojs-demo' exact component={GoJsDemo} />
                 </Switch>
-            </BrowserRouter>
+            </Router>
         )
     }
 }
