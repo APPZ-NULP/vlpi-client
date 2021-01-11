cda3ac6d4fff5194455df17f18b3dce9deec42be
Yurii <yurdosii.ksv@gmail.com>
2021-01-11 20:55:08 +0200

    feat: added TaskCompletePage structure

--- vlpi/src/routers.js
+++ vlpi/src/routers.js
@@ -8,6 +8,7 @@ import GoJsDemo from "./components/GoJsDemo/GoJsDemo";
 import Header from "./components/Header/Header";
 import history from "./history"
 import TaskCreatePage from "./components/TaskCreatePage/TaskCreatePage"
+import TaskCompletePage from "./components/TaskCompletePage/TaskCompletePage"
 import SignIn from './components/LoginPage/LoginPage';
 import TaskStatistics from './components/TaskStatistics/TaskStatistics';
 import UserProfilePage from './components/UserProfilePage/UserProfilePage';
@@ -24,6 +25,7 @@ class Routers extends Component {
                     <Route path='/task-list' exact component={TaskListPage} />
                     <Route path='/gojs-demo' exact component={GoJsDemo} />
                     <Route path='/task-create' exact component={TaskCreatePage} />
+                    <Route path='/tasks/:id/complete' exact component={TaskCompletePage} />
                     <Route path='/login' exact component={SignIn} />
                     <Route path='/sign-up' exact component={SignUp}/>
                     <Route path='/task/:id/statistics' exact component={TaskStatistics}/>
